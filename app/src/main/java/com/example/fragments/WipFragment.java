6f4c6acaa8018c7f5ff75e0786643dc938ddf946
Pavel <pavel4567@gmail.com>
2017-09-24 03:19:35 +0300

    *Set account lobby functional

---
+++ app/src/main/java/com/example/fragments/WipFragment.java
@@ -0,0 +1,23 @@
+package com.example.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pavel.cvwizard.R;
+
+/**
+ * Created by Pavel on 9/24/2017.
+ */
+
+public class WipFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return View.inflate(getContext(), R.layout.wip_layout,null);
+    }
+}
--- app/src/main/java/com/example/pavel/cvwizard/WelcomeScreen.java
+++ app/src/main/java/com/example/pavel/cvwizard/WelcomeScreen.java
@@ -2,12 +2,14 @@ package com.example.pavel.cvwizard;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.AppCompatTextView;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
@@ -20,7 +22,6 @@ import com.example.fragments.TemplateScreen;
 public class WelcomeScreen extends AppCompatActivity {
 
     Button mStartBtn;
-    AppCompatEditText mFilenameField;
     public static Context context;
 
 
@@ -34,13 +35,32 @@ public class WelcomeScreen extends AppCompatActivity {
 
         mStartBtn = (Button) findViewById(R.id.startBtn);
 
-        mStartBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),AccountLobby.class);
-                startActivity(intent);
+        mStartBtn.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        v.getBackground().setColorFilter(0xe0f47521, PorterDuff.Mode.SRC_ATOP);
+                        v.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP: {
+                        v.getBackground().clearColorFilter();
+                        v.invalidate();
+                        Intent intent = new Intent(getApplicationContext(),AccountLobby.class);
+                        startActivity(intent);
+                        break;
+                    }
+                }
+                return false;
             }
         });
+
+      /*  mStartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });*/
     }
 
 
